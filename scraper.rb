80d36dda84dbec1b70d6752550e46b4055fcf488
Daniel Alder <daniel-alder@gmx.net>
2026-04-05 21:33:55 +0200

    Add error handling and moving sqlite file

--- scraper.rb
+++ scraper.rb
@@ -3,12 +3,17 @@ system("wget", "-q", "https://bootstrap.pypa.io/pip/3.6/get-pip.py")
 system("python3 get-pip.py")
 system("/app/.local/bin/pip --version")
 system("git clone https://github.com/daald-docker/sac_uto_touren.git --single-branch --branch main-python-2026 scraper-py")
+system("mv data.sqlite scraper-py/")
 puts ""
+
+data.sqlite
 Dir.chdir("scraper-py") do
   print "Revision: "
   system("git rev-parse HEAD")
   puts ""
   system("/app/.local/bin/pip install -r requirements.txt")
   puts ""
-  system("python3 scraper.py")
+  success = system("python3 scraper.py")
+  exit(1) unless success
 end
+system("mv scraper-py/data.sqlite .")
