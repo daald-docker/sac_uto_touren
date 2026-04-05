c32f7d5a1c9a4fcac2c4bc2f628fa9d75afc0162
Daniel Alder <daniel-alder@gmx.net>
2026-04-05 20:09:41 +0200

    Finalize for morph.io

--- scraper.rb
+++ scraper.rb
@@ -1,13 +1,9 @@
 system("python3 --version")
-#system("pip3 --version")
 system("wget", "-q", "https://bootstrap.pypa.io/pip/3.6/get-pip.py")
 system("python3 get-pip.py")
 system("/app/.local/bin/pip --version")
 system("git clone https://github.com/daald-docker/sac_uto_touren.git --single-branch --branch main-python-2026 scraper-py")
 Dir.chdir("scraper-py") do
-  #system("git branch -a")
-  #system("git remote -v")
   system("/app/.local/bin/pip install -r requirements.txt")
   system("python3 scraper.py")
-  #system("find / -name pip -executable -type f")
 end
