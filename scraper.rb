ad020448cd42e47ed137287b15a09c350410a656
Daniel Alder <daniel-alder@gmx.net>
2026-04-05 19:20:35 +0200

    more

--- scraper.rb
+++ scraper.rb
@@ -1,12 +1,12 @@
 system("python3 --version")
 #system("pip3 --version")
-system("wget", "https://bootstrap.pypa.io/pip/3.6/get-pip.py")
+system("wget", "-q", "https://bootstrap.pypa.io/pip/3.6/get-pip.py")
 system("python3 get-pip.py")
-system("pip --version")
+system("/app/.local/bin/pip --version")
 system("git clone https://github.com/daald-docker/sac_uto_touren.git --single-branch --branch main-python scraper-py")
 Dir.chdir("scraper-py") do
   #system("git branch -a")
   #system("git remote -v")
-  system("pip --version")
+  system("/app/.local/bin/pip install -r requirements.txt")
   #system("find / -name pip -executable -type f")
 end
