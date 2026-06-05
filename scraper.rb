6d5c4b512bd168c5ca9e02afba50f3054710f839
Daniel Alder <daald@users.noreply.github.com>
2026-06-05 22:27:29 +0200

    Fix git checkout

--- scraper.rb
+++ scraper.rb
@@ -1,14 +1,17 @@
 system("wget -qO- https://astral.sh/uv/install.sh | sh") || abort("ERROR while installing uv")
 ENV['PATH'] = "#{File.expand_path('~/.local/bin')}:#{ENV['PATH']}"
 
-system("git remote set-branches --add origin main-python-2026")  # repo was fetched using --single-branch
-system("git fetch")
-system("git checkout main-python-2026") || abort("ERROR while checking out branch")
-
+#system("git remote set-branches --add origin main-python-2026")  # repo was fetched using --single-branch
+#system("git fetch")
+#system("git checkout main-python-2026") || abort("ERROR while checking out branch")
+system("git clone https://github.com/daald-docker/sac_uto_touren.git --single-branch --branch main-python-2026 scraper-py") || abort("ERROR while cloning repo")
+system("cp data.sqlite scraper-py/")
 puts ""
 
-print "Revision: "
-system("git rev-parse HEAD")
-
-puts ""
-success = system("uv run scraper.py") || abort("ERROR while running script")
+Dir.chdir("scraper-py") do
+  print "Revision: "
+  system("git rev-parse HEAD")
+  puts ""
+  system("uv run scraper.py") || abort("ERROR while running script")
+end
+system("mv scraper-py/data.sqlite .")
