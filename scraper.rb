84cdc358a36e23994104300ef9b5d4b42b1456c7
Daniel Alder <daald@users.noreply.github.com>
2026-06-05 22:16:06 +0200

    Change script to uv

--- scraper.rb
+++ scraper.rb
@@ -1,19 +1,14 @@
-system("python3 --version")
-system("wget", "-q", "https://bootstrap.pypa.io/pip/3.6/get-pip.py")
-system("python3 get-pip.py")
-system("/app/.local/bin/pip --version")
-system("git clone https://github.com/daald-docker/sac_uto_touren.git --single-branch --branch main-python-2026 scraper-py")
-system("mv data.sqlite scraper-py/")
+system("wget -qO- https://astral.sh/uv/install.sh | sh") || abort("ERROR while installing uv")
+ENV['PATH'] = "#{File.expand_path('~/.local/bin')}:#{ENV['PATH']}"
+
+system("git remote set-branches --add origin main-python-2026")  # repo was fetched using --single-branch
+system("git fetch")
+system("git checkout main-python-2026") || abort("ERROR while checking out branch")
+
 puts ""
 
-data.sqlite
-Dir.chdir("scraper-py") do
-  print "Revision: "
-  system("git rev-parse HEAD")
-  puts ""
-  system("/app/.local/bin/pip install -r requirements.txt")
-  puts ""
-  success = system("python3 scraper.py")
-  exit(1) unless success
-end
-system("mv scraper-py/data.sqlite .")
+print "Revision: "
+system("git rev-parse HEAD")
+
+puts ""
+success = system("uv run scraper.py") || abort("ERROR while running script")
